656a009d410f9358933268b0921ae1e654ac2694
Stephane Goodwin <goodwin1u@etu.univ-lorraine.fr>
2019-10-15 13:12:35 +0200

    Création classe java

---
+++ PartieInformatique/src/Controller/BoutonLecture_Pause.java
@@ -0,0 +1,5 @@
+package Controller;
+
+public class BoutonLecture_Pause {
+
+}
--- PartieInformatique/src/VisualsMusic.java
+++ PartieInformatique/src/VisualsMusic.java
@@ -1,6 +1,6 @@
 import View.InterfaceGraphique;
 
-public class Main {
+public class VisualsMusic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
