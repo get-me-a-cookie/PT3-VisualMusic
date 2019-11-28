f737f97b2c895d4dd079a631fcf198fe35a14e01
OceanePerrin <oceane.perrin6@etu.univ-lorraine.fr>
2019-11-28 17:02:16 +0100

    Couleur 2D

--- PartieInformatique/src/View/Vue_2D.java
+++ PartieInformatique/src/View/Vue_2D.java
@@ -76,8 +76,17 @@ public class Vue_2D extends JPanel implements Observer {
 		for (int i = MILIEU_FENETRE_X-EPAISSEUR_RECTANGLE*3; 
 				 i < MILIEU_FENETRE_X+EPAISSEUR_RECTANGLE*3; 
 				 i += EPAISSEUR_RECTANGLE) {
+			// On trace le rectangle
+			// la couleur correspond au dedans du rectangle
+			g.setColor(Color.cyan);
 			if (ratioFrequence[j] != 0)
-				g.drawRect(i,
+				g.fillRect(i,
+					   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
+					   EPAISSEUR_RECTANGLE,
+					   (int) (ratioFrequence[j]*MILIEU_FENETRE_Y));
+			// on trace le contour
+			g.setColor(Color.black);
+			g.drawRect(i,
 					   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
 					   EPAISSEUR_RECTANGLE,
 					   (int) (ratioFrequence[j]*MILIEU_FENETRE_Y));
