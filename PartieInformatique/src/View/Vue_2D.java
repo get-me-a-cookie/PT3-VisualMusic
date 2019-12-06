38790d113e6adfe0269e4b34a0b96ee1f67545de
Goodwin Stéphane <goodwinstephane1@gmail.com>
2019-12-06 08:59:06 +0100

    Affichage centré relatif au nb de rectangle finis

--- PartieInformatique/src/View/Vue_2D.java
+++ PartieInformatique/src/View/Vue_2D.java
@@ -5,7 +5,6 @@ import java.awt.Graphics;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import Model.Model;
@@ -23,7 +22,7 @@ public class Vue_2D extends JPanel implements Observer {
 	 * sur l'axe des abscisses (horizontal)
 	 */
 	private static int TAILLE_FENETRE_X = 800;
-	
+
 	/**
 	 * Taille Maximale de la fenètre de l'application
 	 * sur l'axe des ordonnées (vertical)
@@ -62,18 +61,18 @@ public class Vue_2D extends JPanel implements Observer {
 	 * Nombre de rectangle à afficher
 	 */
 	private static int NOMBRE_RECTANGLE =
-            (
-            		(TAILLE_FENETRE_X - 2*MARGIN_FORME_FENETRE)
-            		- 2 * EPAISSEUR_RECTANGLE
-            )
-            / EPAISSEUR_RECTANGLE;
-	
+			(
+					(TAILLE_FENETRE_X - 2*MARGIN_FORME_FENETRE)
+					- 2 * EPAISSEUR_RECTANGLE
+					)
+			/ EPAISSEUR_RECTANGLE;
+
 	/**
 	 * Contient tout les ratios qui seront affiché (barres)
 	 * Taille définit dans méthode update
 	 */
 	private double[] ratioFrequence = new double[
-                             (TAILLE_FENETRE_X - 200) / EPAISSEUR_RECTANGLE
+	                                             (TAILLE_FENETRE_X - 200) / EPAISSEUR_RECTANGLE
 	                                             ];
 
 	/**
@@ -81,7 +80,7 @@ public class Vue_2D extends JPanel implements Observer {
 	 * au nombre de rectangle
 	 */
 	private Color[] couleurs = new Color[NOMBRE_RECTANGLE];
-	
+
 	/**
 	 * Définition de la méthode paint
 	 * Affiche des formes géométriques (2D, 3D)
@@ -92,121 +91,102 @@ public class Vue_2D extends JPanel implements Observer {
 		//Nettoie la fenêtre / l'affichage précedent
 		g.clearRect(0, 0, TAILLE_FENETRE_X, TAILLE_FENETRE_Y);
 
-		// affiche une ligne au centre de la fenêtre
-		/*g.drawLine(100, 
-				   MILIEU_FENETRE_Y, 
-				   TAILLE_FENETRE_X - 100, 
-				   MILIEU_FENETRE_Y);*/
-		
 		//Affiche une ligne epaisse au centre de la fenêtre
 		g.fillRect(MARGIN_FORME_FENETRE, 
-				   MILIEU_FENETRE_Y - EPAISSEUR_LIGNE/2, 
-				   TAILLE_FENETRE_X - 2*MARGIN_FORME_FENETRE, 
-				   EPAISSEUR_LIGNE);
-		
+				MILIEU_FENETRE_Y - EPAISSEUR_LIGNE/2, 
+				TAILLE_FENETRE_X - 2*MARGIN_FORME_FENETRE, 
+				EPAISSEUR_LIGNE);
+
 		//g.setColor(Color.cyan);
-		
+
 		if ((NOMBRE_RECTANGLE % 2) == 0) {
-			System.out.println("Affiche bien -> nb paire");
 			paintPaire(g);
 		}
 		else {
 			paintImpaire(g);
 		}
-		
+
 	}
-	
+
+	/**
+	 * Centre l'affichage des rectangle si le nombre de rectangle
+	 * à affiché est un nombre paire
+	 * @param g : de type Graphics, sert à tracé les rectangles
+	 */
 	private void paintPaire(Graphics g) {
-		int j = 0;
-		for (int i = MARGIN_FORME_FENETRE + EPAISSEUR_RECTANGLE; //MILIEU_FENETRE_X-EPAISSEUR_RECTANGLE*3; 
-				 j < NOMBRE_RECTANGLE; //MILIEU_FENETRE_X+EPAISSEUR_RECTANGLE*3; 
-				 i += EPAISSEUR_RECTANGLE) {
+		int j;
+		j = 0;
+		for (int i = TAILLE_FENETRE_X / 2
+				- (EPAISSEUR_RECTANGLE * NOMBRE_RECTANGLE / 2); //MILIEU_FENETRE_X-EPAISSEUR_RECTANGLE*3; 
+				j < NOMBRE_RECTANGLE / 2; //MILIEU_FENETRE_X+EPAISSEUR_RECTANGLE*3; 
+				i += EPAISSEUR_RECTANGLE) {
 			
-			//Génération d'une couleur différente à chaque fois
-			couleurs[j] = new Color(
-					(float) Math.random(),
-					(float) Math.random(),
-					(float) Math.random());
-			// On trace le rectangle
-			// la couleur correspond au dedans du rectangle
-			g.setColor(couleurs[j]);
-			
-			if (ratioFrequence[j] != 0) {
-				//TODO Avoir lequel vous préférer
-				//choix 1
-				/*
-				 g.fillRect(i,
-						   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
-						   EPAISSEUR_RECTANGLE,
-						   (int) (ratioFrequence[j]*MILIEU_FENETRE_Y));
-						   */
-				
-				//choix 2
-				/*
-				 g.fillRect(i,
-						   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
-						   EPAISSEUR_RECTANGLE,
-						   (int) (ratioFrequence[j]*MILIEU_FENETRE_Y));
-				 g.fillRect(i,
-						   MILIEU_FENETRE_Y+EPAISSEUR_LIGNE/2,
-						   EPAISSEUR_RECTANGLE,
-						   (int) (ratioFrequence[j]*MILIEU_FENETRE_Y));
-						   */
-				
-				//choix 3
-				g.fillRect(i,
-						   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
-						   EPAISSEUR_RECTANGLE,
-						   (int) (ratioFrequence[j]*TAILLE_FENETRE_Y));
-			}
-			
-			// on trace le contour
-			/* TODO INUTILE
-			g.setColor(Color.black);
-			g.drawRect(i,
-					   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
-					   EPAISSEUR_RECTANGLE,
-					   (int) (ratioFrequence[j]*MILIEU_FENETRE_Y));
-		   */
-			j++;
-		}
-	}
-	
-	
-	private void paintImpaire(Graphics g) {
-		
-		System.out.println(NOMBRE_RECTANGLE/2);
-		
-		int j = 0;
-		for (int i = MILIEU_FENETRE_X+EPAISSEUR_RECTANGLE/2;
-				 j < NOMBRE_RECTANGLE/2+1;
-				 i += EPAISSEUR_RECTANGLE) {
-			
-			//Génération d'une couleur différente à chaque fois
-			couleurs[j] = new Color(
-					(float) Math.random(),
-					(float) Math.random(),
-					(float) Math.random());
-			// On trace le rectangle
-			// la couleur correspond au dedans du rectangle
-			g.setColor(couleurs[j]);
-			
-			if (ratioFrequence[j] != 0) {
-				g.fillRect(i-EPAISSEUR_RECTANGLE*((NOMBRE_RECTANGLE/2+1)-j),
-						   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
-						   EPAISSEUR_RECTANGLE,
-						   (int) (ratioFrequence[j]*TAILLE_FENETRE_Y));
-			}
-			
-			if (ratioFrequence[j+(NOMBRE_RECTANGLE/2)] != 0 && j != NOMBRE_RECTANGLE/2) {
-				g.fillRect(i,
-						   (int) (MILIEU_FENETRE_Y-ratioFrequence[j]*MILIEU_FENETRE_Y),
-						   EPAISSEUR_RECTANGLE,
-						   (int) (ratioFrequence[j]*TAILLE_FENETRE_Y));
-			}
-			
+			paintRectCouleur(g, i, j);
 			j ++;
 			
+		}
+		
+		for (int i = TAILLE_FENETRE_X / 2; //MILIEU_FENETRE_X-EPAISSEUR_RECTANGLE*3; 
+				j < NOMBRE_RECTANGLE ; //MILIEU_FENETRE_X+EPAISSEUR_RECTANGLE*3; 
+				i += EPAISSEUR_RECTANGLE) {
+			
+			paintRectCouleur(g, i, j);
+			j ++;
+			
+		}
+			
+	}
+
+	/**
+	 * Centre l'affichage des rectangle si le nombre de rectangle
+	 * à affiché est un nombre impaire
+	 * @param g : de type Graphics, sert à tracé les rectangles
+	 */
+	private void paintImpaire(Graphics g) {
+
+		int j;
+		j = 0;
+		for (int i = TAILLE_FENETRE_X / 2 //- EPAISSEUR_RECTANGLE / 2
+				- (EPAISSEUR_RECTANGLE * NOMBRE_RECTANGLE / 2); //MILIEU_FENETRE_X-EPAISSEUR_RECTANGLE*3; 
+				j < NOMBRE_RECTANGLE / 2; //MILIEU_FENETRE_X+EPAISSEUR_RECTANGLE*3; 
+				i += EPAISSEUR_RECTANGLE) {
+			
+			paintRectCouleur(g, i, j);
+			j ++;
+			
+		}
+		
+		for (int i = TAILLE_FENETRE_X / 2 - EPAISSEUR_RECTANGLE / 2; //MILIEU_FENETRE_X-EPAISSEUR_RECTANGLE*3; 
+				j < NOMBRE_RECTANGLE ; //MILIEU_FENETRE_X+EPAISSEUR_RECTANGLE*3; 
+				i += EPAISSEUR_RECTANGLE) {
+			
+			paintRectCouleur(g, i, j);
+			j ++;
+			
+		}
+
+	}
+
+	/**
+	 * Trace un rectangle avec les coordonnés fournis
+	 * et lui fournis un couleur spécifique
+	 * @param g : de type Graphics, sert à tracé les rectangles
+	 * @param x : l'absisce où commencé à tracer le rectangle
+	 * @param numero_rectangle : Quel rectangle doit être affiché
+	 */
+	private void paintRectCouleur(Graphics g, int x, int numero_rectangle) {
+		// On trace le rectangle
+		// la couleur correspond au dedans du rectangle
+		g.setColor(new Color(
+				(float) Math.random(),
+				(float) Math.random(),
+				(float) Math.random()));
+
+		if (ratioFrequence[numero_rectangle] != 0) {
+			g.fillRect(x,
+					(int) (MILIEU_FENETRE_Y-ratioFrequence[numero_rectangle]*MILIEU_FENETRE_Y),
+					EPAISSEUR_RECTANGLE,
+					(int) (ratioFrequence[numero_rectangle]*TAILLE_FENETRE_Y));
 		}
 		
 	}
@@ -219,7 +199,7 @@ public class Vue_2D extends JPanel implements Observer {
 	 */
 	public void update(Observable m, Object obj) {
 		Model model = (Model) m;
-		
+
 		if (model.getErreur() == null) {
 			for (int index = 0; index < ratioFrequence.length; index ++) {
 				try {
@@ -232,5 +212,5 @@ public class Vue_2D extends JPanel implements Observer {
 			repaint();
 		}
 	}
-	
+
 }
