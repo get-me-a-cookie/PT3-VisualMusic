2e3fb65d245d3422a0dd2a4bd9f220fc37b009bf
Stephane Goodwin <goodwin1u@etu.univ-lorraine.fr>
2019-12-10 14:42:54 +0100

    Saut de ligne ADAPTATEUR

--- PartieInformatique/src/Controller/Adapteur_ControllerVue.java
+++ PartieInformatique/src/Controller/Adapteur_ControllerVue.java
@@ -14,39 +14,47 @@ import javax.swing.JComponent;
 
 
 /*
+ * TODO
  * Moyen que la classe soit inutile
  * Set directement dans les COntroller
  */
 /**
- * @author goodw
  * Classe permettant de faire le lien entre l'interface graphique
  * et les controller
  * 
  * Exemple : Quand on clique sur Stop, on doit modifier 
  * le bouton play ou pause. Pour retrouver le bouton on l'ajoute à
  * cette classe
+ * 
+ * @author goodw
  */
 public class Adapteur_ControllerVue {
+	
 	// Declaration des variables
+	
 	/**
 	 * Ensemble de Composant graphiques
 	 * regroupant tous les élément que l'on doit relié aux controlleur
 	 */
-	Set<Component> component;
+	private Set<Component> component;
 
 	/**
 	 * Constructeur principal
 	 * qui créer une hahset pour instancié l'ensemble de composant
 	 */
 	public Adapteur_ControllerVue() {
+		
 		component = new HashSet<Component>();
+		
 	}
 	/**
 	 * permet de connaitre l'ensemble de composant
 	 * et ainsi de le parcourir afin de trouvé un élément particulier
 	 */
 	public Set<Component> getComponent() {
+		
 		return component;
+		
 	}
 	
 }
