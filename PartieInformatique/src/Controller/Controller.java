6a28607232c893b6c8c403eb8c2ae7fbcf519837
Goodwin <goodwinstephane1@gmail.com>
2020-01-11 20:07:43 +0100

    Correctyion classe Controller

--- PartieInformatique/src/Controller/Controller.java
+++ PartieInformatique/src/Controller/Controller.java
@@ -10,7 +10,9 @@ import Model.Model;
  * Définit un Model qui sera commun à tout les Controller
  * 	et un Constructeur initialisant ce model
  * 
- * @author goodw
+ * @author
+ * Goodwin
+ * 	Création et implémentation de la classe entière
  */
 public abstract class Controller {
 
@@ -23,6 +25,7 @@ public abstract class Controller {
 	/**
 	 * Constructeur instanciant le Modèle permettant la 
 	 * 	structure MVC
+	 * 
 	 * @param model   : Instanciant le Model
 	 */
 	public Controller(Model model) {
